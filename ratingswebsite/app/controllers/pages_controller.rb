a3006a7d11d154bd6132831f1b7d5126ba9fbb02
s.a.f.billings@gmail.com <s.a.f.billings@gmail.com>
2015-04-12 15:37:54 -0400

    Improved feel of tables in views, fixed search methods

--- ratingswebsite/app/controllers/pages_controller.rb
+++ ratingswebsite/app/controllers/pages_controller.rb
@@ -14,25 +14,27 @@ class PagesController < ActionController::Base
         if params[:aname] != nil
             restaurantNameResult = ActiveRecord::Base.connection.execute("SELECT name FROM restaurants WHERE id = " + params[:aname])
             restaurantNameResult.each do |restaurant|
-                restaurantName = restaurant["name"]
+               @restaurantName = restaurant["name"]
             end
-                
         end
         if params[:achoice] == "1"
-            qSPart1 = "SELECT R.type, R.url, L.first_open_date, L.manager_name, L.phone_number, L.street_address, L.hour_open, L.hour_close FROM restaurant R, location L WHERE R.name = "
-            qSPart2 = " AND R.restaurantID= L.restaurantID"
-            # @results = ActiveRecord::Base.connection.execute(qSPart1 + restaurantName + qSPart2)
-            result = ActiveRecord::Base.connection.execute("SELECT * FROM restaurants")
-        elsif params[:achoice] == "2"
-            qSPart1 = "SELECT M.name, M.price FROM menuItem M, restaurant R WHERE R.name = "
-            qSPart2 = " AND M.restaurantID = R.restaurantID"
-            #@results = ActiveRecord::Base.connection.execute(qSPart1 + restaurantName + qSPart2)
-            result = ActiveRecord::Base.connection.execute("SELECT * FROM menuitems")
-        elsif params[:achoice] == "3"
-            qSPart1 = "SELECT M.name, M.price, M.category, M.type, M.description, L.manager_name, L.hour_open, R.url FROM menuItem M, location L, restaurant R WHERE R.name = "
-            qSPart2 = "AND L.restaurantID = R.restaurantID AND M.restaurantID = R.restaurantID AND M.price = (SELECT max(M1.price) FROM menuItem M1 WHERE M1.restaurantID = M.restaurantID)"
-            # @results = ActiveRecord::Base.connection.execute(qSPart1 + restaurantName + qSPart2)
-            result = ActiveRecord::Base.connection.execute("SELECT * FROM restaurants")
+            qSPart1 = "SELECT R.rtype, R.url, L.opening_date, L.manager_name, L.phone_number, L.street_address, L.hour_open, L.hour_close FROM restaurants R, locations L WHERE R.name = "
+            qSPart2 = " AND R.id= L.restaurant_id"
+            # result = ActiveRecord::Base.connection.execute("SELECT * FROM restaurants")
+            result = ActiveRecord::Base.connection.execute(qSPart1 + "\'" + @restaurantName + "\'" + qSPart2)
+        end
+        if params[:achoice] == "2"
+            qSPart1 = "SELECT M.name, M.price FROM menuitems M, restaurants R WHERE R.name = "
+            qSPart2 = " AND M.restaurant_id = R.id"
+            # result = ActiveRecord::Base.connection.execute("SELECT * FROM menuitems")
+            result = ActiveRecord::Base.connection.execute(qSPart1 + "\'" + @restaurantName + "\'" + qSPart2)
+            
+        end
+        if params[:achoice] == "3"
+            qSPart1 = "SELECT M.name, M.price, M.mcategory, M.mtype, M.description, L.manager_name, L.hour_open, R.url FROM menuitems M, locations L, restaurants R WHERE R.name = "
+            qSPart2 = "AND L.restaurant_id = R.id AND M.restaurant_id = R.id AND M.price = (SELECT max(M1.price) FROM menuitems M1 WHERE M1.restaurant_id = M.restaurant_id)"
+            #result = ActiveRecord::Base.connection.execute("SELECT * FROM restaurants")
+            result = ActiveRecord::Base.connection.execute(qSPart1 + "\'" + @restaurantName + "\'" + qSPart2)
         end
         return result
     end
