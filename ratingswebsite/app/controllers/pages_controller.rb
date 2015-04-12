2af806fe6e119fc21d2fe9e353d54f8fad94a66f
s.a.f.billings@gmail.com <s.a.f.billings@gmail.com>
2015-04-12 00:55:48 -0400

    updates

--- ratingswebsite/app/controllers/pages_controller.rb
+++ ratingswebsite/app/controllers/pages_controller.rb
@@ -10,21 +10,28 @@ class PagesController < ActionController::Base
     end
     
     def query_abd
-        if params[:achoice] == "info and location"
+        result = []
+        if params[:aname] != nil
+            restaurantNameResult = ActiveRecord::Base.connection.execute("SELECT name FROM restaurants WHERE id = " + params[:aname])
+            restaurantNameResult.each do |restaurant|
+                restaurantName = restaurant["name"]
+            end
+                
+        end
+        if params[:achoice] == "1"
             qSPart1 = "SELECT R.type, R.url, L.first_open_date, L.manager_name, L.phone_number, L.street_address, L.hour_open, L.hour_close FROM restaurant R, location L WHERE R.name = "
             qSPart2 = " AND R.restaurantID= L.restaurantID"
-            # @results = ActiveRecord::Base.connection.execute(qSPart1 + params[:aname] + qSPart2)
+            # @results = ActiveRecord::Base.connection.execute(qSPart1 + restaurantName + qSPart2)
             result = ActiveRecord::Base.connection.execute("SELECT * FROM restaurants")
-        elsif params[:achoice] == "menu items"
+        elsif params[:achoice] == "2"
             qSPart1 = "SELECT M.name, M.price FROM menuItem M, restaurant R WHERE R.name = "
             qSPart2 = " AND M.restaurantID = R.restaurantID"
-            #@results = ActiveRecord::Base.connection.execute(qSPart1 + params[:aname] + qSPart2)
-            result = ActiveRecord::Base.connection.execute("SELECT * FROM restaurants")
-        else
-            params[:achoice] == "most expensive meal"
+            #@results = ActiveRecord::Base.connection.execute(qSPart1 + restaurantName + qSPart2)
+            result = ActiveRecord::Base.connection.execute("SELECT * FROM menuitems")
+        elsif params[:achoice] == "3"
             qSPart1 = "SELECT M.name, M.price, M.category, M.type, M.description, L.manager_name, L.hour_open, R.url FROM menuItem M, location L, restaurant R WHERE R.name = "
             qSPart2 = "AND L.restaurantID = R.restaurantID AND M.restaurantID = R.restaurantID AND M.price = (SELECT max(M1.price) FROM menuItem M1 WHERE M1.restaurantID = M.restaurantID)"
-            # @results = ActiveRecord::Base.connection.execute(qSPart1 + params[:aname] + qSPart2)
+            # @results = ActiveRecord::Base.connection.execute(qSPart1 + restaurantName + qSPart2)
             result = ActiveRecord::Base.connection.execute("SELECT * FROM restaurants")
         end
         return result
